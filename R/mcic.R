99488cfc444ed639b27dd17d7afb4ccf958b2177
deboonstra <daniel-boonstra@uiowa.edu>
2024-10-10 08:31:29 -0500

    Allowing initial mCIC definition to be employed

--- R/mcic.R
+++ R/mcic.R
@@ -3,14 +3,17 @@
 #' based on Boonstra and Cavanaugh (in works).
 #'
 #' @param object An object of class \code{"geem"} representing the fit.
+#' @param type How mCIC is calculated by using either the model-based covariance
+#' matrix produced by the "unstructured" working correlation structure or the
+#' "robust" covariance matrix produced by the working correlation structure.
 #' @param tol A numeric value specifying the tolerance when calculating the
 #' inverse of the model-based covariance matrix.
 #' Default is \code{.Machine$double.eps}.
 #' @param envir An environment object to which find the \code{"geem"} object.
 #'
-#' @details mCIC is calculated based on the trace of the product of model-based
-#' covariance matrices, where the referent variances are produced by the model
-#' using the unstructured (i.e., general) correlation structure.
+#' @details (Out-dated) mCIC is calculated based on the trace of the product of
+#' model-based covariance matrices, where the referent variances are produced
+#' by the model using the unstructured (i.e., general) correlation structure.
 #'
 #' @return A numeric vector containing the mCIC value for the \code{"geem"}
 #' object under consideration.
@@ -25,7 +28,12 @@
 #' geeM::mcic(fit)
 #'
 #' @export
-mcic <- function(object, tol = .Machine$double.eps, envir = parent.frame()) {
+mcic <- function(
+  object,
+  type = "unstructured",
+  tol = .Machine$double.eps,
+  envir = parent.frame()
+) {
   # Checking function parameter types ####
   if (!methods::is(object, "geem")) {
     stop("object must be an object produced by geeM::geem")
@@ -33,56 +41,74 @@ mcic <- function(object, tol = .Machine$double.eps, envir = parent.frame()) {
   if (!(class(tol) %in% c("numeric", "integer"))) {
     stop("tol must be a numeric or an integer value")
   }
+  if (!(type %in% c("unstructured", "robust"))) {
+    stop("type must be unstructured or type")
+  }
   if (!is.environment(envir)) {
     stop("envir must be an environment object")
   }
 
-  # Pulling modeling call to re-fit models ####
-  model_call <- object$call
+  if (type == "unstructured") {
+    # Pulling modeling call to re-fit models ####
+    model_call <- object$call
 
-  # Assigning corstr to unstructured ####
-  model_call$corstr <- "unstructured"
+    # Assigning corstr to unstructured ####
+    model_call$corstr <- "unstructured"
 
-  # Re-fitting model ####
-  # returning NULL if error or warning occurrs with unstructured model fit
-  model_unstructured <- tryCatch(
-    expr = {
-      model_unstructured <- eval(model_call, envir = envir)
-    },
-    warning = function(w) {
-      NULL
-    },
-    error = function(e) {
-      NULL
-    }
-  )
-
-  if (is.null(model_unstructured)) {
-    stop(
-      paste0(
-        "Unstructured model fit resulted in a warning or error. To examine",
-        " warning or error, fit model outside of function call and specify",
-        " corstr = 'unstructured'."
-      )
+    # Re-fitting model ####
+    # returning NULL if error or warning occurrs with unstructured model fit
+    model_unstructured <- tryCatch(
+      expr = {
+        model_unstructured <- eval(model_call, envir = envir)
+      },
+      warning = function(w) {
+        NULL
+      },
+      error = function(e) {
+        NULL
+      }
     )
+
+    if (is.null(model_unstructured)) {
+      stop(
+        paste0(
+          "Unstructured model fit resulted in a warning or error. To examine",
+          " warning or error, fit model outside of function call and specify",
+          " corstr = 'unstructured'."
+        )
+      )
+    }
+
+    # Getting covariance matrices ####
+
+    ## Inverse of model-based covariance under unstructured ####
+    mb_unstructured <- invert(as.matrix(model_unstructured$naiv.var), tol = tol)
+
+    ## Model-based covariance given working correlation structure ####
+    mb_object <- as.matrix(object$naiv.var)
+
+    ## Checking diagonals ####
+    ## Error if the covariance matrices are non-p.d.
+    if (any(c(diag(mb_unstructured) < 0, diag(mb_object) < 0))) {
+      stop("Non-positive definite covariance matrices present.")
+    }
+
+    # Calculating mCIC ####
+    measure <- sum(diag(x = crossprod(x = mb_unstructured, y = mb_object)))
+  } else if (type == "robust") {
+    # Obtaining covariance matrices ####
+
+    ## Inverse of model-based covariance under working correlation ####
+    omega <- invert(object$naiv.var, tol = tol)
+
+    ## Robust covariance given working correlation structure ####
+    vr <- object$var
+
+    # Calculating mCIC ####
+    measure <- sum(diag(x = crossprod(x = omega, y = vr)))
+  } else {
+    stop("type specified does not allow calculation of mCIC")
   }
-
-  # Getting covariance matrices ####
-
-  ## Inverse of model-based covariance under unstructured ####
-  mb_unstructured <- invert(as.matrix(model_unstructured$naiv.var), tol = tol)
-
-  ## Model-based covariance given working correlation structure ####
-  mb_object <- as.matrix(object$naiv.var)
-
-  ## Checking diagonals ####
-  ## Error if the covariance matrices are non-p.d.
-  if (any(c(diag(mb_unstructured) < 0, diag(mb_object) < 0))) {
-    stop("Non-positive definite covariance matrices present.")
-  }
-
-  # Calculating mCIC ####
-  measure <- sum(diag(x = crossprod(x = mb_unstructured, y = mb_object)))
 
   # Returning mCIC
   return(measure)
